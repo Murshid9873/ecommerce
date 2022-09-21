7f489454077facc1563ec217d1a67eb51933de29
Murshid9873 <Murshid9873@gmail.com>
2022-09-21 11:26:14 +0530

    change

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#akv#c_vd&x9=(24h%z^i@db2+!ocnbl%m-ww7adgz9)i7=rhs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [""]
 
 
 # Application definition
